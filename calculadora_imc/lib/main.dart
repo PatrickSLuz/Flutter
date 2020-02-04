d363de32505b624bfad053378c15c7ffeb7dd6f5
PatrickSLuz <patricksantos20101@gmail.com>
2020-02-04 17:30:46 -0300

    Finalizando o layout do App - Scroll e Padding

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -25,42 +25,48 @@ class _HomeState extends State<Home> {
           ],
         ),
         backgroundColor: Colors.white,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Icon(
-              Icons.person_outline,
-              size: 120,
-              color: Colors.green,
-            ),
-            TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Peso (Kg)",
-                    labelStyle: TextStyle(color: Colors.green)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25)),
-            TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Altura (cm)",
-                    labelStyle: TextStyle(color: Colors.green)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25)),
-            Container(
-                height: 50,
-                child: RaisedButton(
-                  onPressed: () {},
-                  child: Text("Calcular",
-                      style: TextStyle(color: Colors.white, fontSize: 25)),
+        body: SingleChildScrollView(
+            padding: EdgeInsets.fromLTRB(10, 0.0, 10, 0.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(
+                  Icons.person_outline,
+                  size: 120,
                   color: Colors.green,
-                )),
-            Text(
-              "Info",
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.green, fontSize: 25),
-            )
-          ],
-        ));
+                ),
+                TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Peso (Kg)",
+                        labelStyle: TextStyle(color: Colors.green)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25)),
+                TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Altura (cm)",
+                        labelStyle: TextStyle(color: Colors.green)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25)),
+                Padding(
+                  padding: EdgeInsets.only(top: 10, bottom: 10),
+                  child: Container(
+                      height: 50,
+                      child: RaisedButton(
+                        onPressed: () {},
+                        child: Text("Calcular",
+                            style: TextStyle(
+                                color: Colors.white, fontSize: 25)),
+                        color: Colors.green,
+                      )),
+                ),
+                Text(
+                  "Info",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.green, fontSize: 25),
+                )
+              ],
+            )));
   }
 }
