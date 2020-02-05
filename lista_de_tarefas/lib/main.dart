282d4ecc4cc09fa98ab8643344e0936bace01258
PatrickSLuz <patricksantos20101@gmail.com>
2020-02-05 15:45:26 -0300

    Implementado metodos para salvar e ler dados em um arquivo do celular

--- lista_de_tarefas/lib/main.dart
+++ lista_de_tarefas/lib/main.dart
@@ -1,7 +1,52 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(
-    MaterialApp(),
+    MaterialApp(
+      home: Home(),
+    ),
   );
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List _tarefasList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  // Pegar diretorio do arquivo
+  Future<File> _getFile() async {
+    // Pega o diretorio onde eh possivel armazenar os arquivos do app
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data_tarefas.json");
+  }
+
+  // Salvar dados no arquivo
+  Future<File> _saveData() async {
+    // transformando a lista em JSON e armazenando em uma String
+    String data = json.encode(_tarefasList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  // Ler dados gravados no arquivo
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
 }
