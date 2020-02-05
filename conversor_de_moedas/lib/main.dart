077bc3c83a5090e30466fdbf6a39fbe4b5672fa6
PatrickSLuz <patricksantos20101@gmail.com>
2020-02-05 13:27:25 -0300

    Fazendo comunicacao com API - Pegando Resposta

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
 
-void main(){
+const request_url = "https://api.hgbrasil.com/finance?key=b756e362";
+
+void main() async{
+
+  http.Response response = await http.get(request_url);
+  print(json.decode(response.body)["results"]["currencies"]);
+
   runApp(MaterialApp(
     home: Container(),
   ));
