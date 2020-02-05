e6dbb8eba8858c4c667dc07c0ece33228d79f591
PatrickSLuz <patricksantos20101@gmail.com>
2020-02-05 14:37:14 -0300

    Criacao da tela do App

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -1,16 +1,121 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request_url = "https://api.hgbrasil.com/finance?key=b756e362";
 
-void main() async{
-
-  http.Response response = await http.get(request_url);
-  print(json.decode(response.body)["results"]["currencies"]);
-
+void main() async {
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
+    theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white)))),
   ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  double dolar;
+  double euro;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Coversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+        future: getResponse(),
+        builder: (context, snapshot) {
+          // o que vai exibit em cada caso
+          switch (snapshot.connectionState) {
+            // estado da conexao
+            case ConnectionState.none: // nao conectado
+            case ConnectionState.waiting: // esperando conexao
+              return Center(
+                child: Text(
+                  "Carregando Dados...",
+                  style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 25,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            default: // obteve alguma resposta
+              if (snapshot.hasError) {
+                // erro
+                return Center(
+                  child: Text(
+                    "Erro ao Carregar Dados",
+                    style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 25,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                // sucesso
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(Icons.monetization_on,
+                          size: 150, color: Colors.amber),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: "Reais",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefixText: "R\$ "),
+                        style: TextStyle(color: Colors.amber, fontSize: 25),
+                      ),
+                      Divider(), // Espacamento
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: "Dolares",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefixText: "\$ "),
+                        style: TextStyle(color: Colors.amber, fontSize: 25),
+                      ),
+                      Divider(), // Espacamento
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: "Euros",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefixText: "\€ "),
+                        style: TextStyle(color: Colors.amber, fontSize: 25),
+                      ),
+                    ],
+                  ),
+                );
+              }
+          } // switch
+        }, // builder
+      ),
+    );
+  }
+}
+
+Future<Map> getResponse() async {
+  http.Response response = await http.get(request_url);
+  return json.decode(response.body);
 }
