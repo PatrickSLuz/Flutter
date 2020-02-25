6df9ab45ea7552d6b2172ca082ae11bbdb3f2693
PatrickSLuz <patricksantos20101@gmail.com>
2020-02-25 18:37:09 -0300

    Implementado interacao com a camera do celular

--- agenda_contatos/lib/ui/contact_page.dart
+++ agenda_contatos/lib/ui/contact_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:async/async.dart';
 import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ContactPage extends StatefulWidget {
   final Contact contact;
@@ -76,6 +77,18 @@ class _ContactPageState extends State<ContactPage> {
                               ? FileImage(File(_editedContact.img))
                               : AssetImage("images/default.png"))),
                 ),
+                onTap: () {
+                  //ImagePicker.pickImage(source: ImageSource.gallery)
+                  ImagePicker.pickImage(source: ImageSource.camera).then((file) {
+                    if (file == null) {
+                      return;
+                    } else {
+                      setState(() {
+                        _editedContact.img = file.path;
+                      });
+                    }
+                  });
+                },
               ),
               TextField(
                 controller: _nameController,
