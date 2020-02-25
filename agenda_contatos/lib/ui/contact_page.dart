d7645221068211b83c89748849f2b312ab09c4d5
PatrickSLuz <patricksantos20101@gmail.com>
2020-02-25 18:02:12 -0300

    Validacao antes de sair da tela de Novo Contato

--- agenda_contatos/lib/ui/contact_page.dart
+++ agenda_contatos/lib/ui/contact_page.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+import 'package:async/async.dart';
 import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
@@ -38,75 +38,114 @@ class _ContactPageState extends State<ContactPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text(_editedContact.name ?? "Novo Contato"),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (_editedContact.name != null && _editedContact.name.isNotEmpty) {
-            // Navigator funciona com o sistema de Pilha
-            // Push coloca mais um elemento
-            // Pop remove o ultimo elemento
-            // Remover a tela atual e voltar para a anterior, passando um paramento "_editedContact"
-            Navigator.pop(context, _editedContact);
-          } else {
-            FocusScope.of(context).requestFocus(_nameFocus);
-          }
-        },
-        child: Icon(Icons.save),
-        backgroundColor: Colors.red,
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          children: <Widget>[
-            GestureDetector(
-              child: Container(
-                width: 140,
-                height: 140,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        image: _editedContact.img != null
-                            ? FileImage(File(_editedContact.img))
-                            : AssetImage("images/default.png"))),
+    return WillPopScope(
+      onWillPop: () => _requestPop(),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text(_editedContact.name ?? "Novo Contato"),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if (_editedContact.name != null && _editedContact.name.isNotEmpty) {
+              // Navigator funciona com o sistema de Pilha
+              // Push coloca mais um elemento
+              // Pop remove o ultimo elemento
+              // Remover a tela atual e voltar para a anterior, passando um paramento "_editedContact"
+              Navigator.pop(context, _editedContact);
+            } else {
+              FocusScope.of(context).requestFocus(_nameFocus);
+            }
+          },
+          child: Icon(Icons.save),
+          backgroundColor: Colors.red,
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              GestureDetector(
+                child: Container(
+                  width: 140,
+                  height: 140,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: _editedContact.img != null
+                              ? FileImage(File(_editedContact.img))
+                              : AssetImage("images/default.png"))),
+                ),
               ),
-            ),
-            TextField(
-              controller: _nameController,
-              focusNode: _nameFocus,
-              decoration: InputDecoration(labelText: "Nome"),
-              onChanged: (text) {
-                _userEdited = true;
-                setState(() {
-                  _editedContact.name = text;
-                });
-              },
-            ),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: "E-mail"),
-              onChanged: (text) {
-                _userEdited = true;
-                _editedContact.email = text;
-              },
-              keyboardType: TextInputType.emailAddress,
-            ),
-            TextField(
-              controller: _phoneController,
-              decoration: InputDecoration(labelText: "Telefone"),
-              onChanged: (text) {
-                _userEdited = true;
-                _editedContact.phone = text;
-              },
-              keyboardType: TextInputType.phone,
-            ),
-          ],
+              TextField(
+                controller: _nameController,
+                focusNode: _nameFocus,
+                decoration: InputDecoration(labelText: "Nome"),
+                onChanged: (text) {
+                  _userEdited = true;
+                  setState(() {
+                    _editedContact.name = text;
+                  });
+                },
+              ),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: "E-mail"),
+                onChanged: (text) {
+                  _userEdited = true;
+                  _editedContact.email = text;
+                },
+                keyboardType: TextInputType.emailAddress,
+              ),
+              TextField(
+                controller: _phoneController,
+                decoration: InputDecoration(labelText: "Telefone"),
+                onChanged: (text) {
+                  _userEdited = true;
+                  _editedContact.phone = text;
+                },
+                keyboardType: TextInputType.phone,
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<bool>_requestPop() {
+    // Se editou algum campo
+    if (_userEdited){
+      showDialog(context: context,
+      builder: (context){
+        return AlertDialog(
+          title: Text("Descartar Alterações?"),
+          content: Text("Se sair as alterações serão perdidas."),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Cancelar"),
+              onPressed: (){
+                // Fechar o Dialog
+                Navigator.pop(context);
+              }
+            ),
+            FlatButton(
+              child: Text("Sim"),
+                onPressed: (){
+                // Fechar o Dialog
+                Navigator.pop(context);
+                // Fechar a tela de Novo Contato
+                Navigator.pop(context);
+                }
+            ),
+          ],
+        );
+      });
+      // Nao sair automaticamente
+      return Future.value(false);
+    } else {
+      // Sair automaticamente
+      return Future.value(true);
+    }
   }
 }
