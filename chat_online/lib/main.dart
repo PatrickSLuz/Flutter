c52116ae04acbab9b542d26d9f3a3e99bfa5f2dc
PatrickSLuz <patricksantos20101@gmail.com>
2020-03-07 16:27:53 -0300

    Configuracao do Firebase

--- chat_online/lib/main.dart
+++ chat_online/lib/main.dart
@@ -1,5 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp());
+
+  // Testar config com o Firebase
+  Firestore.instance
+      .collection("col")
+      .document("doc")
+      .setData({"texto": "teste"});
 }
