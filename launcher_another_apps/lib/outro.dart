81e6e3f6ab31b68231915b119f3129190e3a87ed
PatrickSLuz <patricksantos20101@gmail.com>
2020-04-10 15:43:35 -0300

    fixes

--- launcher_another_apps/lib/outro.dart
+++ launcher_another_apps/lib/outro.dart
@@ -10,21 +10,21 @@ Widget buildLaucherOutro () {
           keyboardType: TextInputType.text,
           enabled: false,
           decoration: InputDecoration.collapsed(
-            hintText: "Abrir o PigBem",
+            hintText: "Abrir o APP",
           ),
         ),
       ),
       IconButton(
         icon: Icon(Icons.send),
         onPressed: () async {
-          var browser_url = "https://www.pigbem.com";
-          var app_url = "pigbem://app";
+          var browser_url = "https://teste_app";
+          var app_url = "teste_app://app";
 
           if (await canLaunch(app_url))
             await launch(app_url);
 
-//          else if (await canLaunch(browser_url))
-//            await launch(browser_url);
+          else if (await canLaunch(browser_url))
+            await launch(browser_url);
 
           else
             throw "Couldn't launch Outro";
