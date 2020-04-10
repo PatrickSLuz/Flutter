1a2d0ebd93e1f2253f86dbafe25f4447b8e3f935
PatrickSLuz <patricksantos20101@gmail.com>
2020-04-10 17:08:31 -0300

    organizando exibicao de produto em grid e list

--- loja_virtual/lib/tiles/category_tile.dart
+++ loja_virtual/lib/tiles/category_tile.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/category_screen.dart';
 
 class CategoryTile extends StatelessWidget {
 
@@ -19,7 +20,9 @@ class CategoryTile extends StatelessWidget {
       title: Text(snapshot.data["title"]),
       trailing: Icon(Icons.keyboard_arrow_right),
       onTap: () {
-
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => CategoryScreen(snapshot)
+        ));
       },
     );
   }
