7c31afe054003eb987fcdda074f250594d6ed6a2
PatrickSLuz <patricksantos20101@gmail.com>
2020-04-10 21:25:31 -0300

    exibindo produtos por grid

--- loja_virtual/lib/tiles/product_tile.dart
+++ loja_virtual/lib/tiles/product_tile.dart
@@ -4,12 +4,52 @@ import 'package:loja_virtual/datas/product_data.dart';
 class ProductTile extends StatelessWidget {
 
   final String type;
-  final ProductData data;
+  final ProductData product;
 
-  ProductTile(this.type, this.data);
+  ProductTile(this.type, this.product);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+      child: Card(
+        child: type == "gird" ?
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 0.8,
+              child: Image.network(
+                  product.images[0],
+                fit: BoxFit.cover,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      product.title,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500
+                      ),
+                    ),
+                    Text(
+                      "R\$ ${product.price.toStringAsFixed(2)}",
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold
+                      )
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        )
+        : Row(),
+      ),
+    );
   }
 }
