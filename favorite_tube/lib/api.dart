cf1c667fc518d10cbfcbab523524db7a8eac170f
PatrickSLuz <patricksantos20101@gmail.com>
2020-05-23 20:17:35 -0300

    fix api

--- favorite_tube/lib/api.dart
+++ favorite_tube/lib/api.dart
@@ -1,16 +1,31 @@
 import 'dart:convert';
 
 import 'package:favorite_tube/models/video.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:yaml/yaml.dart';
 
 //https://www.googleapis.com/youtube/v3/search?part=snippet&q=$_search&type=video&key=$API_KEY&maxResults=10&pageToken=$_nextToken
 
-const API_KEY = "AIzaSyCUxoF7MY85jesS3RdPWGcwutnJGJRWfno";
-
 class Api {
+  var apiKey;
+
+  Api() {
+    if (apiKey == null) {
+      initApiKey();
+    }
+  }
+
+  initApiKey() async {
+    var envFile = await rootBundle.loadString('env/api_info.yaml');
+    var doc = loadYaml(envFile);
+
+    apiKey = doc['API_KEY'];
+  }
+
   search(String search) async {
     String urlSearch =
-        "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10";
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$apiKey&maxResults=10";
     http.Response response = await http.get(urlSearch);
 
     return decode(response);
